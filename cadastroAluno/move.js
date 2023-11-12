e432505cf96d1689d35fc0a9475af2cec8e5fcc6
jullia <robas.jullia@gmail.com>
2023-11-12 13:15:51 -0300

    continuação desenvolvimento sidebar

--- cadastroAluno/move.js
+++ cadastroAluno/move.js
@@ -1,4 +1,18 @@
 function toggleMenu() {
     var menuLateral = document.getElementById('menuLateral');
-    menuLateral.style.transform = menuLateral.style.transform === 'translateX(0%)' ? 'translateX(-100%)' : 'translateX(0%)';
+    var isVisible = menuLateral.style.transform === 'translateX(0%)';
+
+    // Adiciona ou remove a classe "visible" dependendo do estado atual do menu
+    if (isVisible) {
+        menuLateral.classList.remove('visible', 'align-left');
+    } else {
+        menuLateral.classList.add('visible', 'align-left');
+
+    }
+
+    // Alterna a transformação do menu
+    menuLateral.style.transform = isVisible ? 'translateX(-100%)' : 'translateX(0%)';
+
+
+    
 }
