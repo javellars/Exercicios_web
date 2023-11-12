4fcb824310d686f715f6671b412d9b35e8c0f1b7
jullia <robas.jullia@gmail.com>
2023-11-12 11:50:54 -0300

    tentativa inicial side bar

---
+++ cadastroAluno/move.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menuLateral = document.getElementById('menuLateral');
+    menuLateral.style.transform = menuLateral.style.transform === 'translateX(0%)' ? 'translateX(-100%)' : 'translateX(0%)';
+}
